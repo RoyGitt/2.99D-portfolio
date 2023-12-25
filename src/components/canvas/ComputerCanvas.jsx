9b923a9836679fb403d08b47a2ee32f65283ca83
RoyGitt <officialroy02@gmail.com>
2023-12-26 00:34:40 +0530

    Test

--- src/components/canvas/ComputerCanvas.jsx
+++ src/components/canvas/ComputerCanvas.jsx
@@ -60,6 +60,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: isMobile ? 40 : 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="hidden"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
